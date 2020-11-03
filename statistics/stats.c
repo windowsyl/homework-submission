790a40eb747169258f32f1fbb8ab44ef40e0e25e
windowsyl <ericc@khanlabschool.org>
2020-11-03 08:28:12 -0800

    i really don't remeber how to use git...

---
+++ statistics/stats.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+double *read_array(size_t length)
+{
+    double *nums = malloc(length * sizeof(double));
+    printf("Enter %zu numbers, one per line:\n", length);
+    for(size_t i = 1; i < length; i++)
+    {
+        scanf("%lf\n", &nums[i]);
+    }
+    return nums;
+}
+double sum(const double *nums, size_t length)
+{
+    double sum = 0;
+    for(size_t i = 0; i < length; i++)
+    {
+        sum += nums[i];
+    }
+    return sum;
+}
+double average(const double *nums, size_t length)
+{
+    double sum = 0;
+    for(size_t i = 0; i < length; i++)
+    {
+        sum += nums[i];
+    }
+    return sum/length;
+}
+double stdev(const double *nums, size_t length)
+{
+    double average = 0;
+    for(size_t i = 0; i < length; i++)
+    {
+        average += nums[i];
+    }
+
+    average = average/length;
+    double newsum = 0;
+
+    for(size_t i = 0; i < length; i++)
+    {
+        newsum += (nums[i] - average)*(nums[i] - average);
+    }
+    return sqrt(newsum/length);
+}
+int main( int argc, char *argv[] ) {
+    printf("How many numbers will you enter? ");
+    size_t length;
+    scanf("%zu", &length);
+    double *nums = read_array(length);
+    printf("sum: %lf, average: %lf, stdev: %lf\n", sum(nums, length), average(nums, length), stdev(nums, length));
+    free(nums);
+    
+    return 0;
+}
